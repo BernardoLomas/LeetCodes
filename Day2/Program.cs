4ee557dae540229811aac2e4eb1f16dea419ae4a
BernardoLomas <bernardo.lomasb@gmail.com>
2026-02-03 23:53:18 -0300

    day 2: Two sums leet code

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,18 +1,55 @@
-﻿/* Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+﻿/* Problem Statement
 
-Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-Example 1:
+Rules:
 
-Input: nums = [2,5,1,3,4,7], n = 3
-Output: [2,3,5,4,1,7] 
-Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
-Example 2:
+Each input has exactly one solution
 
-Input: nums = [1,2,3,4,4,3,2,1], n = 4
-Output: [1,4,2,3,3,2,4,1]
-Example 3:
+You may not use the same element twice
 
-Input: nums = [1,1,2,2], n = 2
-Output: [1,2,1,2] */
+You can return the answer in any order
 
+*/
+
+
+class Program
+{
+    public void Main()
+    {
+        Console.WriteLine($"Array length: ");
+        int qntd = int.Parse(Console.ReadLine());
+
+        int [] array = new int [qntd]; 
+
+        for(int i = 0; i < array.length; i++)
+        {
+            Console.WriteLine($"Index {i + 1}/{qntd}: ");
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine($"Target: ");
+        int target = int.Parse(Console.ReadLine());
+        int cont = 0;
+        int [] ArrayTargets = new int [target]; 
+
+        do
+        {
+            for(int i = 0; i < array.lenght; i++)
+            {
+                if(array[i] + cont <= target)
+                {
+                    cont += array[i];
+                    ArrayTargets[i] = array[i];
+                }
+            }
+        } while (cont != target);
+
+        for(int i = 0; i < ArrayTargets.lenght; i++)
+        {
+            Console.WriteLine($"Index: {i} + ");
+        }
+        
+        Console.WriteLine($"Target: {target}");
+    }
+} 
